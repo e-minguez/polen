33eb247976f1adc9e07f1dbe6be367948eb392ba
Eduardo Minguez <e.minguez@gmail.com>
2020-05-23 23:47:06 +0200

    Primer tweet

--- polen.py
+++ polen.py
@@ -1,8 +1,18 @@
-import requests
-import json
+import requests, json, tweepy, os
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
-POLEN_URL = 'http://gestiona.madrid.org/geoserver/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&LAYERS=SPOL_V_CAPTADORES_GIS&QUERY_LAYERS=SPOL_V_CAPTADORES_GIS&STYLES=&BBOX=365560.97254%2C4415910.465472%2C495339.02746%2C4558089.534528&FEATURE_COUNT=50&HEIGHT=493&WIDTH=450&FORMAT=image%2Fpng&INFO_FORMAT=text%2Fhtml&SRS=EPSG%3A23030&X=202&Y=251'
+load_dotenv()
+
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+
+POLEN_URL = os.getenv("POLEN_URL")
+
+sneeze = "\U0001f927"
+
 page = requests.get(POLEN_URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -20,7 +30,28 @@ for i in range(0,len(data),3):
   tmpdict = {}
   tmpdict['tipo'] = data[i]
   tmpdict['medicion'] = data[i+1]
-  tmpdict['nivel'] = data[i+2]
+  #tmpdict['nivel'] = data[i+2]
   dataDict['datos'].append(tmpdict)
 
-print(json.dumps(dataDict,indent=4))
+#print(json.dumps(dataDict,indent=4))
+tweet = sneeze + " " + dataDict['ciudad'] + ": " + dataDict['fecha'] + "\n"
+
+for dic in dataDict['datos']:
+    for k in dic:
+      if k != "medicion":
+        tweet += dic[k] + ": "
+      else:
+        tweet += dic[k] + "\n"
+
+print(tweet)
+#exit()
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+# Create API object
+api = tweepy.API(auth)
+
+# Create a tweet
+api.update_status(tweet)
